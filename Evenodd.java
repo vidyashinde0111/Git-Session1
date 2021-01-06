356b3dce7b21d4fdf989946891660f295f413e3f
vidya <vidya>
2021-01-06 16:54:47 +0530

    this is second commit

--- Evenodd.java
+++ Evenodd.java
@@ -6,8 +6,8 @@ public class Evenodd {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number");
-		int n=sc.nextInt();
-		if(n%2==0)
+		int num=sc.nextInt();
+		if(num%2==0)
 		{
 			System.out.println("number is even");
 		}
--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,6 @@ package practise;
 
 public class Helloworld {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello World");
 	}
 }
